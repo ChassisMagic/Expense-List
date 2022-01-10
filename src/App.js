3a88d5eb762a41287d7a3948e5714c9608fbea2b
noiro-id <kusumalingga@hotmail.com>
2022-01-10 23:50:13 +0700

    implementing loading message while fetching data.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovieHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
-    
+
     const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
