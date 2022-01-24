b37019949d66d578ef09b0ba0161c87a1c71f96a
noiro-id <kusumalingga@hotmail.com>
2022-01-24 11:30:38 +0700

    Implementing custom-hook for new task.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   
 
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = ((taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    });
+
+    fetchTasks({
+      url: "https://noiroid-186905-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    }, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
